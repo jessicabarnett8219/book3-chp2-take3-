baaefb138e998918b571138eeeaddece4d43eba6
jessicabarnett8219 <jessica.barnett8219@gmail.com>
2018-10-11 15:58:49 -0500

    set up object

---
+++ scripts.js
@@ -0,0 +1,45 @@
+// Her congressional district (you can use yours here)
+// Her platform statements for the following issues.
+//      Taxes
+//       Jobs
+//      Infrastructure
+//      Health care
+//      Crime and enforcement
+// URL for donation form
+// Calendar of events
+// Volunteer information
+    // Name
+    // Address
+    // Email
+    // Phone number
+    // Availability
+    // What activities each one is willing to do (e.g. answering phone calls, taking polls, etc.)
+// Biography
+// Image gallery
+//    Head shot
+//    Picture of family
+//    Picture of constituents
+// Mission statement
+// URL for registering to vote
+
+let sangerCampaign = {
+  district: 5,
+  platforms: {
+    taxes: "fdhkjhgfdkjhgkdjfh",
+    jobs: "kjhgkdjfhgkjfdhg",
+    infrastructure: "kjdhfkjdhgkh",
+    healthCare: "fjhgkjfdhgkj",
+    crimeEnforcement: "kjdhkjfdhg"
+  },
+  donationForm: "www.donatesanger.com",
+  eventCalendar: "www.donatesanger.com/events",
+  volunteers: [],
+  biography: "fkjdghkdjfhgkjfdhg",
+  imageGallery: {
+    headShot: "elizabeth.jpg",
+    familyPhoto: "family.jpg",
+    constituentsPhoto: "people.jpg"
+  },
+  missionStatement: ,
+  registerToVote: 
+}
