c23b926bbd16e0df79cd95bb32daad2a51a43319
jessicabarnett8219 <jessica.barnett8219@gmail.com>
2018-10-11 22:26:24 -0500

    finish object

--- scripts.js
+++ scripts.js
@@ -33,13 +33,50 @@ let sangerCampaign = {
   },
   donationForm: "www.donatesanger.com",
   eventCalendar: "www.donatesanger.com/events",
-  volunteers: [],
+  volunteers: [
+    vol1 = {
+      Name: "Jonathan Beam",
+      Address: "3510 Golf St. Nashville, TN 37216",
+      Email: "jonthan.beam@gmail.com",
+      phoneNumber: "859-825-8824",
+      availability: "Days and Weekends",
+      activities: {
+        willCanvas: true,
+        willPhoneBank: false,
+        willPoll: false
+      }
+    }, 
+    vol2 = {
+      Name: "Wally Barnett-Beam",
+      Address: "3500 Golf St. Nashville, TN 37216",
+      Email: "wally@gmail.com",
+      phoneNumber: "859-800-8824",
+      availability: "Evenings",
+      activities: {
+        willCanvas: false,
+        willPhoneBank: false,
+        willPoll: false
+      }
+    }, 
+    vol3 = {
+      Name: "Jessica Barnett",
+      Address: "3600 Golf St. Nashville, TN 37216",
+      Email: "jessica.barnett@gmail.com",
+      phoneNumber: "502-338-0908",
+      availability: "Weekends",
+      activities: {
+        willCanvas: false,
+        willPhoneBank: false,
+        willPoll: true
+      }
+    }
+  ],
   biography: "fkjdghkdjfhgkjfdhg",
   imageGallery: {
     headShot: "elizabeth.jpg",
     familyPhoto: "family.jpg",
     constituentsPhoto: "people.jpg"
   },
-  missionStatement: ,
-  registerToVote: 
+  missionStatement: "jkfgkjfdgh",
+  registerToVote: "ghkjdfhgkjdfhg"
 }
