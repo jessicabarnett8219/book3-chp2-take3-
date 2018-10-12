cedbd373a04bdd4c0173786c2a88a3a46670e145
jessicabarnett8219 <jessica.barnett8219@gmail.com>
2018-10-11 23:05:34 -0500

    fix syntax errors, changed dummy text, add console logs to test

--- scripts.js
+++ scripts.js
@@ -1,40 +1,16 @@
-// Her congressional district (you can use yours here)
-// Her platform statements for the following issues.
-//      Taxes
-//       Jobs
-//      Infrastructure
-//      Health care
-//      Crime and enforcement
-// URL for donation form
-// Calendar of events
-// Volunteer information
-    // Name
-    // Address
-    // Email
-    // Phone number
-    // Availability
-    // What activities each one is willing to do (e.g. answering phone calls, taking polls, etc.)
-// Biography
-// Image gallery
-//    Head shot
-//    Picture of family
-//    Picture of constituents
-// Mission statement
-// URL for registering to vote
-
 let sangerCampaign = {
   district: 5,
   platforms: {
-    taxes: "fdhkjhgfdkjhgkdjfh",
-    jobs: "kjhgkdjfhgkjfdhg",
-    infrastructure: "kjdhfkjdhgkh",
-    healthCare: "fjhgkjfdhgkj",
-    crimeEnforcement: "kjdhkjfdhg"
+    taxes: "high taxes",
+    jobs: "more jobs",
+    infrastructure: "build roads",
+    healthCare: "single payer",
+    crimeEnforcement: "tough on crime"
   },
   donationForm: "www.donatesanger.com",
   eventCalendar: "www.donatesanger.com/events",
   volunteers: [
-    vol1 = {
+    {
       Name: "Jonathan Beam",
       Address: "3510 Golf St. Nashville, TN 37216",
       Email: "jonthan.beam@gmail.com",
@@ -44,39 +20,45 @@ let sangerCampaign = {
         willCanvas: true,
         willPhoneBank: false,
         willPoll: false
+        }
+      },
+      {
+        Name: "Wally Barnett-Beam",
+        Address: "3500 Golf St. Nashville, TN 37216",
+        Email: "wally@gmail.com",
+        phoneNumber: "859-800-8824",
+        availability: "Evenings",
+        activities: {
+          willCanvas: false,
+          willPhoneBank: false,
+          willPoll: false
+        }
+      },
+      {
+        Name: "Jessica Barnett",
+        Address: "3600 Golf St. Nashville, TN 37216",
+        Email: "jessica.barnett@gmail.com",
+        phoneNumber: "502-338-0908",
+        availability: "Weekends",
+        activities: {
+          willCanvas: false,
+          willPhoneBank: false,
+          willPoll: true
+        }
       }
-    }, 
-    vol2 = {
-      Name: "Wally Barnett-Beam",
-      Address: "3500 Golf St. Nashville, TN 37216",
-      Email: "wally@gmail.com",
-      phoneNumber: "859-800-8824",
-      availability: "Evenings",
-      activities: {
-        willCanvas: false,
-        willPhoneBank: false,
-        willPoll: false
-      }
-    }, 
-    vol3 = {
-      Name: "Jessica Barnett",
-      Address: "3600 Golf St. Nashville, TN 37216",
-      Email: "jessica.barnett@gmail.com",
-      phoneNumber: "502-338-0908",
-      availability: "Weekends",
-      activities: {
-        willCanvas: false,
-        willPhoneBank: false,
-        willPoll: true
-      }
-    }
-  ],
+    ],
   biography: "fkjdghkdjfhgkjfdhg",
   imageGallery: {
     headShot: "elizabeth.jpg",
     familyPhoto: "family.jpg",
     constituentsPhoto: "people.jpg"
   },
-  missionStatement: "jkfgkjfdgh",
-  registerToVote: "ghkjdfhgkjdfhg"
+  missionStatement: "elect me",
+  registerToVote: "votetn.org"
 }
+
+;
+
+console.log("high taxes? " + sangerCampaign.platforms.taxes)
+console.log("Jonathan Beam?" + sangerCampaign["volunteers"][0]["Name"])
+console.log("false?" + sangerCampaign["volunteers"][0].activities.willPoll)
